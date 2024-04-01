77b6d41e8e6ffb50cf7e0414d3732fadc9a42834
figeral <mouliofitzgerard@gmail.com>
2024-04-01 23:13:13 +0100

    changes done on url parameters

--- src/bot.py
+++ src/bot.py
@@ -3,13 +3,13 @@ import requests
 import os
 
 
-def send_msg(text: str):
+def send_msg(msg: str):
     token = os.environ['GOMU_BOT_TOKEN']
     chanel_id = os.environ['GOMU_CHANEL_ID']
 
     baseurl = "api.telegram.org"
     url = f"{baseurl}/bot{token}"
-    payload = dict(chat_id=chanel_id, text=text)
+    payload = dict(chat_id=chanel_id, text=msg)
 
     try:
         requests.post(url=url, params=payload)
