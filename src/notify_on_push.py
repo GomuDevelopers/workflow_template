42239d4dcb722706a9335f360c2cd89189ab7426
figeral <mouliofitzgerard@gmail.com>
2024-04-02 10:40:53 +0100

    sleeping the forked branch

--- src/notify_on_push.py
+++ src/notify_on_push.py
@@ -1,18 +1,18 @@
-from bot import send_msg
-import os
-import time
+# from bot import send_msg
+# import os
+# import time
 
 
-def send_msg_on_push():
-    author = os.environ['GITHUB_AUTHOR']
-    repository = os.environ['GITHUB_REPOSITORY']
-    link = f"https://github.com/{repository}"
-    commits = os.environ['GITHUB_COMMIT']
-    # count = len(os.environ['GITHUB_PuSH_NUMBER'])
-    date = time.ctime()
-    text = f"📌 Push effectué par {author} 🧠 \n Date: {date}  \n \n Commits: {commits[:25]} ... \n sentez vous libre l'examiné  {link}"
-    send_msg(text)
+# def send_msg_on_push():
+#     author = os.environ['GITHUB_AUTHOR']
+#     repository = os.environ['GITHUB_REPOSITORY']
+#     link = f"https://github.com/{repository}"
+#     commits = os.environ['GITHUB_COMMIT']
+#     # count = len(os.environ['GITHUB_PuSH_NUMBER'])
+#     date = time.ctime()
+#     text = f"📌 Push effectué par {author} 🧠 \n Date: {date}  \n \n Commits: {commits[:25]} ... \n sentez vous libre l'examiné  {link}"
+#     send_msg(text)
 
 
-if __name__ == '__main__':
-    send_msg_on_push()
+# if __name__ == '__main__':
+#     send_msg_on_push()
