82b9a4413601928b6959eb6651e7a26f76ded7c8
figeral <mouliofitzgerard@gmail.com>
2024-04-02 09:02:49 +0100

    modification done on  push count and commits to text desc

--- src/notify_on_push.py
+++ src/notify_on_push.py
@@ -8,9 +8,9 @@ def send_msg_on_push():
     repository = os.environ['GITHUB_REPOSITORY']
     link = f"https://github.com/{repository}"
     commits = os.environ['GITHUB_COMMIT']
-    count = os.environ['GITHBU_PuSH_NUMBER']
+    count = len(os.environ['GITHBU_PuSH_NUMBER'])
     date = time.ctime()
-    text = f"📌 Push effectué par {author} 🧠 le {date} \n\n Push n*: {count} \n Commits: {commits[:20]}... \n sentez vous libre l'examiné  {link}"
+    text = f"📌 Push effectué par {author} 🧠 \n Date: {date} \n Push n*: {count} \n Commits: {commits[:20]}... \n sentez vous libre l'examiné  {link}"
     send_msg(text)
 
 
